6d6fa03afb34d9ae8ad5995f4eadd303f1a70fc9
Naterra <natalinyweb@hotmail.com>
2020-01-14 12:41:18 -0500

    front page parallax

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
 import { connect } from "react-redux";
+import { Parallax, Background } from 'react-parallax';
 
 /**  Components  **/
 import Layout from '../components/layouts/Layout'
-import Parallax from '../components/Materialize/Parallax/parallax';
+// import Parallax from '../components/Materialize/Parallax/parallax';
 
 
 
@@ -40,9 +41,44 @@ class MainPage extends Component {
 
     return (
       <Layout meta={meta} pageClass="mainPage" frontPage={true} container={false} {...this.props} >
-          <Parallax image="/static/banner/parallaxBanner.jpg" {...this.props} />
+          {/*<Parallax image="/static/banner/parallaxBanner.jpg" {...this.props} />*/}
+          {/*<Parallax image="/static/banner/bg5.5.jpg" {...this.props} />*/}
+
+          <Parallax
+              blur={0}
+              bgImage={('/static/banner/bg9.1.jpg')}
+              // bgImage={('/static/banner/bg5.5.jpg')}
+              bgImageAlt="the cat"
+              strength={200}
+              renderLayer={percentage => (
+                  <div
+                      style={{
+                          position: 'absolute',
+                          background: `rgba(4, 69, 125, 0.57)`,
+                          left: '20%',
+                          top: '27%',
+                          width: '60%',
+                          height: '270px',
+                          color: '#eff8ff',
+                          padding: '10px'
+                      }}
+                  >
+                      <div style={{fontSize:'23px',marginTop: '45px', color:'#fff', textAlign: 'center'}}>Explore the world
+                            <div>of</div>
+                            <div style={{fontSize:'55px', fontStyle: 'italic' }}>Switzerland</div>
+                      </div>
+                      {/*<h3 style={{color:'#fff', textAlign: 'center'}}>of</h3>*/}
+                      {/*<h1 style={{color:'#fff', textAlign: 'center'}}>Switzerland</h1>*/}
+                  </div>
+              )}
+          >
+              {/*Put some text content here - even an empty div with fixed dimensions to have a height*/}
+              {/*for the parallax.*/}
+              <div style={{ height: '400px' }} />
+          </Parallax>
 
           <div className="container">
+              hello
               {record && <div className="row description ckEditorContent" dangerouslySetInnerHTML={{ __html: record.description }} />}
           </div>
 
