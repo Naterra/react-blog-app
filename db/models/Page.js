b2941185d8a07b73f75f83548a5cb44c748b5a12
Naterra <natalinyweb@hotmail.com>
2020-01-17 20:40:30 -0500

    _modelIncrement model schema for auto  incrementing records id

--- db/models/Page.js
+++ db/models/Page.js
@@ -1,13 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const _ModelIncrement = require("./ModelIncrement");
 
 const PageSchema = new Schema({
+    id: { type: Number ,  default: 0},
     title: { type: String , index: { unique: true }},
     description: { type: String },
     image: { type: String },
     showOnMenu: { type: Boolean, default: false },
     showOnFront: { type: Boolean, default: false },
     createdDate: { type: Date, default: Date.now }
+});
+
+
+
+PageSchema.pre('save', async function(next) {
+
+    if (this.isNew) {
+        const id = await _ModelIncrement.getNextId('Page');
+        // console.log('> new id', id);
+
+        this.id = id; // Incremented
+        next();
+    } else {
+        next();
+    }
 });
 
 
